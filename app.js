2ec8faff8a07836577c3c5c3ccf22e0129423100
timand1 <80768929+timand1@users.noreply.github.com>
2022-06-10 17:11:08 +0200

    1

--- app.js
+++ app.js
@@ -10,13 +10,17 @@ app.use(cors());
 const shoppingRouter = require('./routes/shoppinglist')
 app.use('/api/shoppinglist', auth, shoppingRouter)
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept",
-        "Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS"
-    );
+    // res.setHeader("Access-Control-Allow-Origin", "*");
+    // res.header(
+    //     "Access-Control-Allow-Headers",
+    //     "Origin, X-Requested-With, Content-Type, Accept",
+    //     "Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS"
+    // );
     // res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
